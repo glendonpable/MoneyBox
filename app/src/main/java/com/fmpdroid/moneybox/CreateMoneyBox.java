8fdd5b43dfd56809b559ca14c80313fc9a9b109e
Glendon <glendon.pable@gmail.com>
2020-07-20 02:00:06 +0800

    MON-1_Added Create screen with transition

--- app/src/main/java/com/fmpdroid/moneybox/CreateMoneyBox.java
+++ app/src/main/java/com/fmpdroid/moneybox/CreateMoneyBox.java
@@ -3,24 +3,47 @@ package com.fmpdroid.moneybox;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class CreateMoneyBox extends AppCompatActivity {
 
+    private static String PREFERENCE = "moneybox_preference"; //temp variable
+    private static String TEMP_CHECK = "moneybox_isEmpty"; //temp variable
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_create_money_box);
-
-        Toolbar toolbar = findViewById(R.id.toolbar2);
-        toolbar.setTitle("");
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        //toolbar.set("");
-        getSupportActionBar().setDisplayShowTitleEnabled(true);
-        getSupportActionBar().setTitle("test");
-        //toolbar.set(true);
+        getSupportActionBar().setTitle(R.string.title_activity_create_money_box);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
+    }
+
+    public void createMoneyBox(View v){
+
+        Toast.makeText(CreateMoneyBox.this, "No no no", Toast.LENGTH_SHORT).show(); //temp
+        /*
+        SharedPreferences preferences = getApplicationContext().getSharedPreferences(PREFERENCE, 0);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean(TEMP_CHECK, false);
+        editor.commit();
+
+        Intent intent = new Intent(CreateMoneyBox.this, MainActivity.class);
+        startActivity(intent);
+        */
+    }
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 
 
